aacd6e435b5598fb2663e279e6e22bde3345d9ca
Pat Browne <pjbrowne011@gmail.com>
2024-08-30 21:58:06 -0400

    add xforms design with wood mode
    Add buttons to enable metric to imperial conversion. Wood mode is
    for woodworking conversions from mm to in (and back).

---
+++ woodforms/calc_top_cb.c
@@ -0,0 +1,46 @@
+#include "calc_top.h"
+
+/* Callbacks and freeobj handles for form calc_top */
+
+/***************************************
+ ***************************************/
+
+void cb_digit( FL_OBJECT * ob,
+         long        data )
+{
+    /* Fill-in code for callback here */
+}
+
+
+/***************************************
+ ***************************************/
+
+void cb_arith( FL_OBJECT * ob,
+         long        data )
+{
+    /* Fill-in code for callback here */
+}
+
+
+/***************************************
+ ***************************************/
+
+void cb_stack( FL_OBJECT * ob,
+         long        data )
+{
+    /* Fill-in code for callback here */
+}
+
+
+/***************************************
+ ***************************************/
+
+void cb_mode( FL_OBJECT * ob,
+         long        data )
+{
+    /* Fill-in code for callback here */
+}
+
+
+
+
